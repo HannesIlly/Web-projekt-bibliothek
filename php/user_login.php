a9bd606e6cc944a04f51c34f840c9c303fc37d27
Hannes Illy <Hannes.Illy@gmx.de>
2020-05-13 12:14:48 +0200

    Added failed login attempt counter in database.

--- php/user_login.php
+++ php/user_login.php
@@ -13,29 +13,45 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $database = new PDO('mysql:host=localhost; dbname=bibliothek', 'root', '');
 
     // Get user login data
-    $statement = 'SELECT UID, user_pass, role FROM logindaten WHERE user_name = "' . $username . '";';
+    $statement = 'SELECT UID, user_pass, role, user_fehlversuche FROM logindaten WHERE user_name = "' . $username . '";';
     echo $statement;
     $query = $database->query($statement);
     $loginDaten = $query->fetchAll();
 
-    if (count($loginDaten) > 0) {
-        if (password_verify($password, $loginDaten[0]['user_pass'])) {
-            // Get user name
-            $statement = 'SELECT vorname, nachname FROM userdaten WHERE UID = ' . $loginDaten[0]['UID'] . ';';
-            $query = $database->query($statement);
-            $userDaten = $query->fetchAll();
+    $loginAttempts = $loginDaten[0]['user_fehlversuche'];
+    if ($loginAttempts < 3) {
+        if (count($loginDaten) > 0) {
+            if (password_verify($password, $loginDaten[0]['user_pass'])) {
+                // Get user name
+                $statement = 'SELECT vorname, nachname FROM userdaten WHERE UID = ' . $loginDaten[0]['UID'] . ';';
+                $query = $database->query($statement);
+                $userDaten = $query->fetchAll();
 
-            $_SESSION['login'] = 'ok';
-            $_SESSION['username'] = $username;
-            $_SESSION['first-name'] = $userDaten[0]['vorname'];
-            $_SESSION['role'] = $loginDaten[0]['role'];
-            $seite = '../hauptseite.php';
+                $_SESSION['login'] = 'ok';
+                $_SESSION['username'] = $username;
+                $_SESSION['first-name'] = $userDaten[0]['vorname'];
+                $_SESSION['role'] = $loginDaten[0]['role'];
+
+                $loginAttempts = 0;
+
+                $seite = '../hauptseite.php';
+            } else {
+                $loginAttempts++;
+
+                $seite = '../html/login_form.html';
+            }
+
+            // store login attempts
+            $statement = 'UPDATE logindaten SET user_fehlversuche = :user_fehlversuche WHERE UID = ' . $loginDaten[0]['UID'] . ';';
+            $query = $database->prepare($statement);
+            $parameters = ['user_fehlversuche' => $loginAttempts];
+            $query->execute($parameters);
         } else {
-            $seite = '../html/login_form.html';
+            // username not found
+            $seite = '../html/user_register_form.html';
         }
     } else {
-        // username not found
-        $seite = '../html/user_register_form.html';
+
     }
 }
 
